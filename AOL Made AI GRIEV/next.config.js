a32d31eb331bb43c1fbc386daf5cb46669d33c27
foustbrothersllc <foustbrothersllc@gmail.com>
2026-05-12 23:50:20 -0400

    Add files via upload

---
+++ AOL Made AI GRIEV/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse', 'mammoth']
+  },
+  api: {
+    bodyParser: false
+  }
+}
+
+module.exports = nextConfig
